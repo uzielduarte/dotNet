fcad7a55c918b04b860a210c040ed1439891b7a3
uzielduarte <duarteuziel36@gmail.com>
2021-05-18 08:37:13 -0600

    Calculadora en .net

---
+++ LenguageBasic-2m3/LenguageBasic/Form1.Designer.vb
@@ -0,0 +1,367 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.btnPorcentaje = New System.Windows.Forms.Button()
+        Me.btnClearElement = New System.Windows.Forms.Button()
+        Me.btnClearAll = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnArmonica = New System.Windows.Forms.Button()
+        Me.btnPowerOfTwo = New System.Windows.Forms.Button()
+        Me.btnSquare = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnSevent = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.btnMult = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnTree = New System.Windows.Forms.Button()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.FlowLayoutPanel2 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.FlowLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnPorcentaje)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnClearElement)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnClearAll)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnDelete)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnArmonica)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnPowerOfTwo)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnSquare)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnDiv)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnSevent)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnEight)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnNine)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnMult)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnFour)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnFive)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnSix)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnSubtract)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button17)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnTwo)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnTree)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnSum)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button21)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button22)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Button23)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnEqual)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 243)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(336, 176)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'btnPorcentaje
+        '
+        Me.btnPorcentaje.Location = New System.Drawing.Point(3, 3)
+        Me.btnPorcentaje.Name = "btnPorcentaje"
+        Me.btnPorcentaje.Size = New System.Drawing.Size(75, 23)
+        Me.btnPorcentaje.TabIndex = 0
+        Me.btnPorcentaje.Text = "%"
+        Me.btnPorcentaje.UseVisualStyleBackColor = True
+        '
+        'btnClearElement
+        '
+        Me.btnClearElement.Location = New System.Drawing.Point(84, 3)
+        Me.btnClearElement.Name = "btnClearElement"
+        Me.btnClearElement.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearElement.TabIndex = 1
+        Me.btnClearElement.Text = " CE"
+        Me.btnClearElement.UseVisualStyleBackColor = True
+        '
+        'btnClearAll
+        '
+        Me.btnClearAll.Location = New System.Drawing.Point(165, 3)
+        Me.btnClearAll.Name = "btnClearAll"
+        Me.btnClearAll.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearAll.TabIndex = 2
+        Me.btnClearAll.Text = " C"
+        Me.btnClearAll.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(246, 3)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = " <="
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnArmonica
+        '
+        Me.btnArmonica.Location = New System.Drawing.Point(3, 32)
+        Me.btnArmonica.Name = "btnArmonica"
+        Me.btnArmonica.Size = New System.Drawing.Size(75, 23)
+        Me.btnArmonica.TabIndex = 4
+        Me.btnArmonica.Text = " 1/x"
+        Me.btnArmonica.UseVisualStyleBackColor = True
+        '
+        'btnPowerOfTwo
+        '
+        Me.btnPowerOfTwo.Location = New System.Drawing.Point(84, 32)
+        Me.btnPowerOfTwo.Name = "btnPowerOfTwo"
+        Me.btnPowerOfTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnPowerOfTwo.TabIndex = 5
+        Me.btnPowerOfTwo.Text = "x^2"
+        Me.btnPowerOfTwo.UseVisualStyleBackColor = True
+        '
+        'btnSquare
+        '
+        Me.btnSquare.Location = New System.Drawing.Point(165, 32)
+        Me.btnSquare.Name = "btnSquare"
+        Me.btnSquare.Size = New System.Drawing.Size(75, 23)
+        Me.btnSquare.TabIndex = 6
+        Me.btnSquare.Text = "x^(1\2)"
+        Me.btnSquare.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Location = New System.Drawing.Point(246, 32)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(75, 23)
+        Me.btnDiv.TabIndex = 7
+        Me.btnDiv.Text = "÷"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnSevent
+        '
+        Me.btnSevent.Location = New System.Drawing.Point(3, 61)
+        Me.btnSevent.Name = "btnSevent"
+        Me.btnSevent.Size = New System.Drawing.Size(75, 23)
+        Me.btnSevent.TabIndex = 8
+        Me.btnSevent.Text = "7"
+        Me.btnSevent.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Location = New System.Drawing.Point(84, 61)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(75, 23)
+        Me.btnEight.TabIndex = 9
+        Me.btnEight.Text = "8"
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Location = New System.Drawing.Point(165, 61)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(75, 23)
+        Me.btnNine.TabIndex = 10
+        Me.btnNine.Text = "9"
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'btnMult
+        '
+        Me.btnMult.Location = New System.Drawing.Point(246, 61)
+        Me.btnMult.Name = "btnMult"
+        Me.btnMult.Size = New System.Drawing.Size(75, 23)
+        Me.btnMult.TabIndex = 11
+        Me.btnMult.Text = "x"
+        Me.btnMult.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Location = New System.Drawing.Point(3, 90)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(75, 23)
+        Me.btnFour.TabIndex = 12
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Location = New System.Drawing.Point(84, 90)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(75, 23)
+        Me.btnFive.TabIndex = 13
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Location = New System.Drawing.Point(165, 90)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(75, 23)
+        Me.btnSix.TabIndex = 14
+        Me.btnSix.Text = "6"
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(246, 90)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubtract.TabIndex = 15
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(3, 119)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(75, 23)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = "1"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(84, 119)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnTwo.TabIndex = 17
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnTree
+        '
+        Me.btnTree.Location = New System.Drawing.Point(165, 119)
+        Me.btnTree.Name = "btnTree"
+        Me.btnTree.Size = New System.Drawing.Size(75, 23)
+        Me.btnTree.TabIndex = 18
+        Me.btnTree.Text = "3"
+        Me.btnTree.UseVisualStyleBackColor = True
+        '
+        'btnSum
+        '
+        Me.btnSum.Location = New System.Drawing.Point(246, 119)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(75, 23)
+        Me.btnSum.TabIndex = 19
+        Me.btnSum.Text = "+"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(3, 148)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(75, 23)
+        Me.Button21.TabIndex = 20
+        Me.Button21.Text = " +/-"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Location = New System.Drawing.Point(84, 148)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(75, 23)
+        Me.Button22.TabIndex = 21
+        Me.Button22.Text = "0"
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Location = New System.Drawing.Point(165, 148)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(75, 23)
+        Me.Button23.TabIndex = 22
+        Me.Button23.Text = "."
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'btnEqual
+        '
+        Me.btnEqual.Location = New System.Drawing.Point(246, 148)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(75, 23)
+        Me.btnEqual.TabIndex = 23
+        Me.btnEqual.Text = "="
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'FlowLayoutPanel2
+        '
+        Me.FlowLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label1)
+        Me.FlowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft
+        Me.FlowLayoutPanel2.Location = New System.Drawing.Point(3, 211)
+        Me.FlowLayoutPanel2.Name = "FlowLayoutPanel2"
+        Me.FlowLayoutPanel2.Size = New System.Drawing.Size(333, 27)
+        Me.FlowLayoutPanel2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(315, 5)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "0"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 420)
+        Me.Controls.Add(Me.FlowLayoutPanel2)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel2.ResumeLayout(False)
+        Me.FlowLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents btnPorcentaje As Button
+    Friend WithEvents btnClearElement As Button
+    Friend WithEvents btnClearAll As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnArmonica As Button
+    Friend WithEvents btnPowerOfTwo As Button
+    Friend WithEvents btnSquare As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btnSevent As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents btnMult As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnTree As Button
+    Friend WithEvents btnSum As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents btnEqual As Button
+    Friend WithEvents FlowLayoutPanel2 As FlowLayoutPanel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ LenguageBasic-2m3/LenguageBasic/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LenguageBasic")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LenguageBasic")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5a55e0f1-e92a-4ad2-89c1-2d46a0401942")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
