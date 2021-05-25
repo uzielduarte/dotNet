75d31201617c3e96b635c6697168f20c30ac67b6
uzielduarte <duarteuziel36@gmail.com>
2021-05-25 08:37:42 -0600

    Usando switch

---
+++ Online/Online/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Online
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.cmbCategoriaEdad = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edad:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(108, 22);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 1;
+            this.txtEdad.TextChanged += new System.EventHandler(this.TxtEdad_TextChanged);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(33, 128);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // cmbCategoriaEdad
+            // 
+            this.cmbCategoriaEdad.FormattingEnabled = true;
+            this.cmbCategoriaEdad.Items.AddRange(new object[] {
+            "Bebe",
+            "Nino",
+            "Teen",
+            "Joven",
+            "Adulto",
+            "Viejo",
+            "Prehistorico"});
+            this.cmbCategoriaEdad.Location = new System.Drawing.Point(99, 66);
+            this.cmbCategoriaEdad.Name = "cmbCategoriaEdad";
+            this.cmbCategoriaEdad.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategoriaEdad.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 282);
+            this.Controls.Add(this.cmbCategoriaEdad);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cmbCategoriaEdad;
+    }
+}
+
---
+++ Online/Online/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Online
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtEdad_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int edad = getEdad();
+            string categoria = cmbCategoriaEdad.SelectedItem.ToString();
+            string message = "Vacio";
+
+            switch (categoria)
+            {
+                case "Bebe":
+                    if(edadValidacion(edad, 0, 4))
+                    {
+                        message = "La persona es un bebe";
+                    }
+                    break;
+                case "Nino":
+                    if (edadValidacion(edad, 5, 11))
+                    {
+                        message = "La persona es un nino";
+                        MessageBox.Show("Nino");
+                    }
+                    MessageBox.Show("Afuera");
+                    break;
+                case "Teen":
+                    if (edadValidacion(edad, 12, 17))
+                    {
+                        message = "La persona es un teen";
+                    }
+                    break;
+                case "Joven":
+                    if (edadValidacion(edad, 18, 25))
+                    {
+                        message = "La persona es un joven";
+                    }
+                    break;
+                case "Adulto":
+                    if (edadValidacion(edad, 26, 45))
+                    {
+                        message = "La persona es un nino";
+                    }
+                    break;
+                case "Viejo":
+                    if (edadValidacion(edad, 46, 85))
+                    {
+                        message = "La persona es un viejo";
+                    }
+                    break;
+                case "Prehistorico":
+                    if (edadValidacion(edad, 86, int.MaxValue))
+                    {
+                        message = "La persona es prehistorica";
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Ningun case coincide");
+                    break;
+            }
+
+            MessageBox.Show(message + edad);
+            /*if(int.TryParse(txtEdad.Text, out edad))
+            {
+                MessageBox.Show($"Edad: {edad}");
+            }
+            else
+            {
+                MessageBox.Show("ERROR");
+            }*/
+        }
+
+        private int getEdad()
+        {
+            if (int.TryParse(txtEdad.Text, out int edad))
+            {
+                return edad;
+            }
+            return int.MinValue;
+        }
+
+        private bool edadValidacion(int edad, int min, int max)
+        {
+            if(min <= edad && edad <= max)
+            {
+                return true;
+            }
+            MessageBox.Show("Falso");
+            return false;
+        }
+    }
+}
